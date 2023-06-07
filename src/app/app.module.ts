b5969abf53256b3460cf51545a5d89b4a80d6094
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-07 02:26:37 -0500

    imported components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { ExpenseComponent } from './expense/expense.component';
+import { IncomeComponent } from './income/income.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { CategoryComponent } from './category/category.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    ExpenseComponent,
+    IncomeComponent,
+    RegisterComponent,
+    UserDashboardComponent,
+    CategoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HttpClientModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
