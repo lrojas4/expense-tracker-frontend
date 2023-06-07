74885a35e97ec96d7553077d6c04df9ff364f33d
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-07 14:19:28 -0500

    Added route to login

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/api.service';
 import { RegisterUser } from '../models/user.models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,15 @@ import { RegisterUser } from '../models/user.models';
 })
 export class RegisterComponent {
 
-  constructor(private apiService: ApiService){}
+  constructor(private apiService: ApiService, private router: Router){}
 
   user : RegisterUser = {name: '', email: '', password: ''};
 
   onSubmit(){
-    this.apiService.registerUser(this.user).subscribe((data : any) => {localStorage.setItem('jwt',data.message);
-    console.log(localStorage)} )
+    this.apiService.registerUser(this.user).subscribe((data : any) => {
+      localStorage.setItem('jwt',data.message);
+      console.log(localStorage);
+      this.router.navigate(['/login']);
+    });
   }
 }
