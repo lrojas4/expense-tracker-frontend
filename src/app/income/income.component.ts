a9c983a32edf9e8386373518286e6eb4658a7ba5
Lorena Rojas <102558556+lrojas4@users.noreply.github.com>
2023-06-08 16:29:46 -0500

    added deleteIncome()

--- src/app/income/income.component.ts
+++ src/app/income/income.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { tap } from 'rxjs';
 import { ApiService } from 'src/api.service';
 
 @Component({
@@ -9,12 +11,31 @@ import { ApiService } from 'src/api.service';
 export class IncomeComponent implements OnInit {
 
   incomes: any;
+  income: any;
+  id:any;
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.apiService.getIncomes().subscribe((data:any) => {this.incomes = data;
       console.log(data)});
+
+    const isIdPresent = this.activatedRoute.snapshot.paramMap.has('id');
+      if (isIdPresent) {
+        const id = this.activatedRoute.snapshot.paramMap.get('id');
+        this.apiService.getIncome(this.id).subscribe(
+        data => this.income = data 
+        )
+      }
+  }
+
+  deleteIncome(id: number) {
+    this.apiService.deleteIncome(id).pipe(
+      tap((response: any) => {
+        console.log('response', response);
+        this.router.navigateByUrl('/incomes');
+      })
+    ).subscribe();
   }
 
 }
